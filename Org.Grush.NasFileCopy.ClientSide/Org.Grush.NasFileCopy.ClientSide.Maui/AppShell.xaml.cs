ad9f9601e2ef9114aea5261f3b61da1c85c8188e
Samuel K. Grush <Samuel@Grush.org>
2024-06-08 15:36:04 -0500

    init maui

---
+++ Org.Grush.NasFileCopy.ClientSide/Org.Grush.NasFileCopy.ClientSide.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Org.Grush.NasFileCopy.ClientSide.Maui;
+
+public partial class AppShell : Shell
+{
+  public AppShell()
+  {
+    InitializeComponent();
+  }
+}
---
+++ Org.Grush.NasFileCopy.ClientSide/Org.Grush.NasFileCopy.ClientSide.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Org.Grush.NasFileCopy.ClientSide.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+  protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
