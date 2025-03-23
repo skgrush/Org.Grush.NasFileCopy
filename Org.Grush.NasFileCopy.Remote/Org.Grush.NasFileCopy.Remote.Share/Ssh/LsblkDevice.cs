d84caae4781a58adb77230dd26f3c1026d45484a
Samuel K. Grush <Samuel@Grush.org>
2025-03-22 22:20:27 -0500

    lsblk docs

--- Org.Grush.NasFileCopy.Remote/Org.Grush.NasFileCopy.Remote.Share/Ssh/LsblkDevice.cs
+++ Org.Grush.NasFileCopy.Remote/Org.Grush.NasFileCopy.Remote.Share/Ssh/LsblkDevice.cs
@@ -4,50 +4,57 @@ using System.Text.Json.Serialization;
 namespace Org.Grush.NasFileCopy.Remote.Share.Ssh;
 
 
+/// <summary>
+/// Entry in lsblk called with the args <see cref="Options"/>.
+/// </summary>
+/// <param name="Path">device path e.g. '/dev/sda' or '/dev/sda1'</param>
+/// <param name="Ro">readonly</param>
+/// <param name="Hotplug">Hotpluggable</param>
+/// <param name="Vendor">human-readable hardware manufacturer name, only applied to disks sometimes even if model is applied</param>
+/// <param name="Model">human-readable hardware device name, only applied to disks sometimes, even without vendor</param>
+/// <param name="Label">user-defined UI label</param>
+/// <param name="Name"></param>
+/// <param name="Partlabel">special name for stuff like "Microsoft reserved partition"</param>
+/// <param name="Parttype">the UUID of the partition type</param>
+/// <param name="Parttypename">the human-readable name of the partition type</param>
+/// <param name="Size">size in bytes; safely fits in JSON max safe int = 9_007_199_254_740_991 = 9 PB</param>
+/// <param name="Mountpoint">
+/// null if unmounted.
+/// string path to mountpoint.
+/// square-bracketed string for virtual mounts like "[SWAP]".
+/// </param>
+/// <param name="Type">e.g. "disk" or "part" or "crypt"</param>
+/// <param name="Tran">transport, e.g. "usb" or "nvme", but can be null including for some USB partitions or virtuals</param>
+/// <param name="Fstype">filesystem type, e.g.  "vfat"  or  "zfs_member"  or  "swap"  or  "exfat"</param>
+/// <param name="Fsver"> file system, e.g.      "FAT32" or  "5000"        or  "1"     or  "1.0"</param>
+/// <param name="Mountpoints">List of all current mountpoints. Will be <c>[null]</c> if no mountpoints.</param>
+/// <param name="Children">All child devices of this device.</param>
 public record struct LsblkDevice(
-  // device path e.g. '/dev/sda' or '/dev/sda1'
   string Path,
-  // readonly
   bool Ro,
-  // Hotpluggable
   bool Hotplug,
-  // human-readable hardware manufacturer name, only applied to disks sometimes even if model is applied
   string? Vendor,
-  // human-readable hardware device name, only applied to disks sometimes, even without vendor
   string? Model,
-  // user-defined UI label
   string? Label,
   string Name,
-  // special name for stuff like "Microsoft reserved partition"
   string? Partlabel,
-  // the UUID of the partition type
   string? Parttype,
-  // the human-readable name of the partition type
   string? Parttypename,
-  // size in bytes; safely fits in JSON max safe int = 9_007_199_254_740_991 = 9 PB
+  string? Partuuid,
   ulong Size,
-  // null if unmounted
-  // string path to mountpoint
-  // square-bracketed string for virtual mounts like "[SWAP]"
   string? Mountpoint,
-  // e.g. "disk" or "part" or "crypt"
   string Type,
-  // transport, e.g. "usb" or "nvme", but can be null including for some USB partitions or virtuals
   string Tran,
-  // filesystem type, e.g.  "vfat"  or  "zfs_member"  or  "swap"  or  "exfat"
   string Fstype,
-  // file system, e.g.      "FAT32" or  "5000"        or  "1"     or  "1.0"
   string Fsver,
   ImmutableArray<string?> Mountpoints,
   ImmutableArray<LsblkDevice>? Children
 )
 {
-  /// <summary>
-  /// Print bytes as a number
-  /// </summary>
+  /// <summary>Print bytes as a number</summary>
   public const string Options = $"--bytes --json --paths --output {OutputColumns}";
   public const string OutputColumns =
-    "PATH,RO,HOTPLUG,VENDOR,MODEL,LABEL,NAME,PARTLABEL,PARTTYPE,PARTTYPENAME,SIZE,MOUNTPOINT,TYPE,TRAN,FSTYPE,FSVER,MOUNTPOINTS";
+    "PATH,RO,HOTPLUG,VENDOR,MODEL,LABEL,NAME,PARTLABEL,PARTTYPE,PARTTYPENAME,PARTUUID,SIZE,MOUNTPOINT,TYPE,TRAN,FSTYPE,FSVER,MOUNTPOINTS";
 
   public record struct LsblkResult(
     ImmutableArray<LsblkDevice> Blockdevices
